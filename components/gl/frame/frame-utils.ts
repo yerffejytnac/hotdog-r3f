bdf06407e99fa508116d4de38ab56c67b4893c9e
Jeffrey Canty <jeffrey@tremendous.dev>
2025-09-19 16:11:09 -0500

    fix ts error

--- components/gl/frame/frame-utils.ts
+++ components/gl/frame/frame-utils.ts
@@ -1,22 +1,30 @@
-import type { Vector3D } from "@/types/frame"
+type Vector3D = {
+  x: number;
+  y: number;
+  z: number;
+};
 
 export const toRadians = (degrees: Vector3D): [number, number, number] => {
-  const rad = Math.PI / 180
-  return [degrees.x * rad, degrees.y * rad, degrees.z * rad]
-}
+  const rad = Math.PI / 180;
+  return [degrees.x * rad, degrees.y * rad, degrees.z * rad];
+};
 
-export const calculateRailPositions = (width: number, height: number, profileWidth: number) => {
-  const halfW = width / 2
-  const halfH = height / 2
-  const halfProfile = profileWidth / 2
+export const calculateRailPositions = (
+  width: number,
+  height: number,
+  profileWidth: number,
+) => {
+  const halfW = width / 2;
+  const halfH = height / 2;
+  const halfProfile = profileWidth / 2;
 
   return {
     top: { x: 0, y: halfH + halfProfile, z: 0 },
     bottom: { x: 0, y: -halfH - halfProfile, z: 0 },
     left: { x: -halfW - halfProfile, y: 0, z: 0 },
     right: { x: halfW + halfProfile, y: 0, z: 0 },
-  }
-}
+  };
+};
 
 export const calculateRailScales = (
   width: number,
@@ -25,19 +33,19 @@ export const calculateRailScales = (
   profileDepth: number,
   originalSize: { x: number; y: number; z: number },
 ) => {
-  const horizLen = width + profileWidth * 2
-  const vertLen = height + profileWidth * 2
+  const horizLen = width + profileWidth * 2;
+  const vertLen = height + profileWidth * 2;
 
   return {
-    horizontal: [horizLen / originalSize.x, profileWidth / originalSize.y, profileDepth / originalSize.z] as [
-      number,
-      number,
-      number,
-    ],
-    vertical: [vertLen / originalSize.x, profileWidth / originalSize.y, profileDepth / originalSize.z] as [
-      number,
-      number,
-      number,
-    ],
-  }
-}
+    horizontal: [
+      horizLen / originalSize.x,
+      profileWidth / originalSize.y,
+      profileDepth / originalSize.z,
+    ] as [number, number, number],
+    vertical: [
+      vertLen / originalSize.x,
+      profileWidth / originalSize.y,
+      profileDepth / originalSize.z,
+    ] as [number, number, number],
+  };
+};
